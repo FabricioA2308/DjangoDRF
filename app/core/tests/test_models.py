5c9bf0d5f9264eaa9c8801431d20dbf97e24ef0a
Fabricio <fabriciolmd2308@gmail.com>
2024-11-21 16:11:17 -0300

    Added user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,31 @@
+# Test for models.
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_succesfully(self):
+        email = "test@example.com"
+        password = "testpassword"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_is_normalized(self):
+        # Test if email is normalized to newly created user
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
